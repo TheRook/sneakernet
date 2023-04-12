036dd04a3ef62ed5e3113ccd17a182783bba3cd2
TheRook <firealwaysworks@gmail.com>
2023-04-12 12:31:34 -0700

    smaller build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+
 const fs = require('fs');
 var HtmlWebpackInlineSourcePlugin = require('@effortlessmotion/html-webpack-inline-source-plugin');
 
@@ -17,7 +20,6 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
         title: "zip",
-        //hash: true,
         inject: "body",
         // filename: './dist/index.html', //relative to root of the application
         inlineSource: '.(js|css)$' // embed all javascript and css inline        
@@ -43,5 +45,21 @@ module.exports = {
   // pass all js files through Babel
   resolve: {
     extensions: [".js", "*.css"],
-  }  
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new HtmlMinimizerPlugin({
+        test: /\.(html)$/,
+      }),
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+      })  
+    ],
+  },  
 }
